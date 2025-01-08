dfdb8507a443743aee633d2422b4ac1953813f13
Rafael Rojas Diaz <118632412+Raghart@users.noreply.github.com>
2025-01-07 21:27:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname)));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 let persons = [
